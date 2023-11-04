f86386ae6baf09640502d2ba3ac67fed7e606c66
Habtamu Dires <habtamuadugna.d@gmail.com>
2023-11-04 12:22:39 +0300

    using kubernetes

--- clients/src/main/java/org/example/clients/notification/NotificationClient.java
+++ clients/src/main/java/org/example/clients/notification/NotificationClient.java
@@ -3,9 +3,13 @@ package org.example.clients.notification;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient("notification")
+@FeignClient(
+        name = "notification",
+        url="${clients.notification.url}"
+)
 public interface NotificationClient {
 
     @PostMapping("api/v1/notifications")
     void sendNotification(NotificationRequest notificationRequest);
+
 }
